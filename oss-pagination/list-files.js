fe6360206ae81d1bd08714d46c8a16391ac27af2
Neil Spink <neilspink@gmail.com>
2019-12-16 17:39:38 +0800

    working FC

--- oss-pagination/list-files.js
+++ oss-pagination/list-files.js
@@ -1,54 +1,65 @@
 'use strict';
 /*
-    Alibaba Cloud: List objects in an OSS
+    Alibaba Cloud: Find and list object from an OSS that have a last modified timestame of today.
 */
 
-const OSS = require('ali-oss');
+const OSS = require('ali-oss').Wrapper;
 
 const oss_region = 'oss-cn-shanghai';
 const bucket_name = '<yourBucketName>';
 const object_prefix = '';
-const accessKeyId = '<yourAccessKeyId>';
-const accessKeySecret = '<yourAccessKeySecret>';
 
-const store = new OSS({
-    region: oss_region,
-    internal: false,
-    secure: true,
-    accessKeyId: accessKeyId,
-    accessKeySecret: accessKeySecret,
-    bucket: bucket_name
-});
+async function objectsModifiesToday(context) {
 
-async function listFiles1() {
-    const result = await store.list();
-    console.log(result);
-}
-
-//listFiles1();
-
-async function listFiles2() {
+    const today = new Date();
 
     let truncated = false;
     let nextMarker = '';
+    let result = [];
+
+    const store = new OSS({
+        region: oss_region,
+        internal: true,
+        secure: true,
+        accessKeyId: context.credentials.accessKeyId,
+        accessKeySecret: context.credentials.accessKeySecret,
+        stsToken: context.credentials.securityToken,
+        bucket: bucket_name
+    });
 
     do {
         const objectsRequest = {
-            Marker: nextMarker,
             Prefix: object_prefix,
-            MaxKeys: 100
-        };
+            marker: nextMarker,
+            ['max-keys']: 100
+        }
 
-        console.log(objectsRequest);
-        const result = await store.list(objectsRequest);
-        console.log(result.nextMarker);
-        console.log('************************************');
+        const search = await store.list(objectsRequest);
+
+        search.objects.forEach(item => {
+
+            let lastModified = new Date(Date.parse(item.lastModified));
+
+            if (today.toDateString() === lastModified.toDateString()) {
+                result.push(item);
+            }
+        });
 
         truncated = result.isTruncated;
         nextMarker = result.nextMarker;
     } while (truncated === true)
+
+    return result;
 }
 
-listFiles2();
+module.exports.handler = async function(event, context, callback) {
+    console.log('running...');
 
+    await objectsModifiesToday(context).then(files => {
+        console.log(files);
+    }).catch(ex => {
+        console.log(ex)
+    });
 
+    callback(null, 'end');
+}
